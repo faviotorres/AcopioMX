459ecf54674aa94c4a23714f8087300e31d380d9
faviotorres <favinhio3@gmail.com>
2017-10-04 16:07:35 -0700

    mapbox token

---
+++ app/src/main/java/com/faviotorres/acopiomx/Client.java
@@ -0,0 +1,7 @@
+package com.faviotorres.acopiomx;
+
+public class Client {
+
+    public static final String MAPBOX_TOKEN = "pk.eyJ1IjoiZmF2ZWNhIiwiYSI6ImNqODVpN2F1NDBsNWwzMn" +
+            "BkZGFtZmhqNnYifQ.3HHOhB0m6SDhFuAQvmDlEg";
+}
